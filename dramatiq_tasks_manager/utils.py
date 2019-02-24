b7125a5431c7f26dae9760c22b2e466e7a9711d6
cthtuf <gdazeysinc@gmail.com>
2019-02-24 21:09:19 +0500

    Fix request on task execution. Add cache to getactors-methods. Set new version 0.0.3. Close #1

--- dramatiq_tasks_manager/utils.py
+++ dramatiq_tasks_manager/utils.py
@@ -1,12 +1,18 @@
 import logging
+from functools import lru_cache
 
 from django.conf import settings
 
+from apscheduler.util import ref_to_obj as getactorfunc_by_path
+from dramatiq.actor import Actor
 from redis import Redis
+
+from typing import Union
 
 logger = logging.getLogger(__name__)
 
 
+@lru_cache(maxsize=1)
 def get_declared_actors() -> list:
     """
     Get the list of declared actors from Redis.
@@ -21,7 +27,8 @@ def get_declared_actors() -> list:
         return []
 
 
-def get_actor_apschedulerpath_by_name(actor_name: str) -> str:
+@lru_cache(maxsize=1)
+def get_actor_apschedulerpath_by_name(actor_name: str) -> Union[str, None]:
     """
     Get the path where actor is located. It needs to apscheduler when it scheduling a job.
     :param actor_name:
@@ -32,4 +39,20 @@ def get_actor_apschedulerpath_by_name(actor_name: str) -> str:
         return redis_obj.hget(settings.REDIS_ACTORS_LIST_HASH, actor_name)
     except Exception:
         logger.exception('Cant get actor data')
-        return ""
+        return None
+
+
+@lru_cache(maxsize=1024)
+def getactor_by_name(actor_name: str) -> Union[Actor, None]:
+    """
+    Get Actor method by actor_name
+    :param actor_name:
+    :return:
+    """
+    actor_path = None
+    try:
+        actor_path = get_actor_apschedulerpath_by_name(actor_name)
+        return getactorfunc_by_path(actor_path)
+    except Exception:
+        logger.exception('Cant get actor by name', extra={'actor_name': actor_name, 'actor_path': actor_path})
+        return None
