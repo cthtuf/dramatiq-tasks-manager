1a7f0a6960c302b0cccd4b996b499758f421186e
cthtuf <gdazeysinc@gmail.com>
2019-03-19 18:05:50 +0500

    Set version 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dramatiq-tasks-manager',
-    version='0.0.10',
+    version='0.0.11',
     packages=['dramatiq_tasks_manager',
               'dramatiq_tasks_manager.management',
               'dramatiq_tasks_manager.management.commands',
